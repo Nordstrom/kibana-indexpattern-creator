d80458a887b9ab079f0159b684663efa8a71a495
Ravi Rajaram <ravichandran.rajaram@nordstrom.com>
2018-03-11 22:13:02 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import {resolve} from 'path';
+import api from './server/routes';
+
+export default function (kibana) {
+    return new kibana.Plugin({
+        require: ['elasticsearch'],
+
+        uiExports: {
+
+            app: {
+                title: 'Create Index Pattern',
+                description: 'This plugin allow you to create index pattern if it does not exist',
+                main: 'plugins/create_index_pattern/app',
+                icon: 'plugins/create_index_pattern/images/icon.svg'
+            }
+
+        },
+
+        config(Joi) {
+            return Joi.object({
+                enabled: Joi.boolean().default(true),
+            }).default();
+        },
+
+
+        init(server, options) {
+            api(server);
+        }
+
+
+    });
+};
---
+++ public/app.js
@@ -0,0 +1,64 @@
+const uiModules = require('ui/modules');
+
+import uiRoutes from 'ui/routes';
+import  _ from "lodash";
+
+
+import 'ui/autoload/styles';
+import './less/main.less';
+import {CreateIndexPatternService} from './create-index-pattern-service'
+import template from './templates/index.html';
+uiRoutes.enable();
+uiRoutes
+    .when('/', {
+        template: template,
+        controller: 'createIndexPattern',
+        controllerAs: 'ctrl'
+    })
+
+uiModules
+    .get('app/create_index_pattern', [])
+    .controller('createIndexPattern', function ($scope, $location, $route, $interval, $http) {
+
+        $scope.title = 'Create IndexPattern';
+        $scope.finishLoad = false;
+        $scope.description = 'Create IndexPattern';
+        $scope.icon = "";
+
+        $scope.location = $location;
+        $scope.$watch('location.search()', function() {
+            $scope.target = ($location.search()).target;
+        }, true);
+
+        $scope.changeTarget = function(name) {
+            $location.search('target', name);
+        }
+
+        $scope.init = function () {
+            $scope.target = ($location.search()).target;
+            var request = {'timeFieldName': '@timestamp', 'title': $scope.target};
+            $http.post('../api/getIndexPattern', request).then((response) => {
+              $scope.found = response.data.found;
+              if($scope.found){
+                $scope.message = 'already exists'
+                $scope.createmessage = 'to view'
+              }else{
+                $scope.message = 'does not exist'
+                $scope.createmessage = 'to create one'
+              }
+           });
+        }
+
+    $scope.createPattern = function () {
+      var baseurl = $location.$$absUrl.split('/app')[0]
+      var request = {'timeFieldName': '@timestamp', 'title': $scope.target};
+      if($scope.found){
+        $http.post('../api/createShortUrl', request).then((response) => {
+           window.location.href=baseurl + "/goto/shorturl" + $scope.target
+        });
+      }else{
+        $http.post('../api/createIndexPattern', request).then((response) => {
+        });
+      }
+    }
+});
